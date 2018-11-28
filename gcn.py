d0d9a9b10fbe1776b831a9171f76a23fed668109
zoharli <zoharg@outlook.com>
2018-11-28 16:55:50 +0800

    add gcn.py

---
+++ gcn.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+from torch.nn import init
+import torch.nn.functional as F
+
+import numpy as np
+
+# GCN basic operation
+class GCNBlock(nn.Module):
+    def __init__(self, input_dim, output_dim, add_self=False, normalize_embedding=False,
+            dropout=0.0, bias=True):
+        super().__init__()
+        self.add_self = add_self
+        self.dropout = dropout
+        if dropout > 0.001:
+            self.dropout_layer = nn.Dropout(p=dropout)
+        self.normalize_embedding = normalize_embedding
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.weight = nn.Parameter(torch.FloatTensor(input_dim, output_dim).cuda())
+        if bias:
+            self.bias = nn.Parameter(torch.FloatTensor(output_dim).cuda())
+        else:
+            self.bias = None
+
+    def forward(self, x, adj):
+        if self.dropout > 0.001:
+            x = self.dropout_layer(x)
+        y = torch.matmul(adj, x)
+        if self.add_self:
+            y += x
+        y = torch.matmul(y,self.weight)
+        if self.bias is not None:
+            y = y + self.bias
+        if self.normalize_embedding:
+            y = F.normalize(y, p=2, dim=2)
+        return y
+
