f2cd02085aaec857f6b7f40498a6fabe252f5f7e
zoharli <zhanghengli@pku.edu.cn>
2019-01-19 12:13:22 +0800

    f

--- neibor_att_lrelu.sh
+++ neibor_att_lrelu.sh
@@ -2,10 +2,8 @@
 
 for i in $(seq $1 $2)
 do
-python3 main.py -fold=$i -softmax=neibor -adj_norm=none -dnorm=1 -relu=lrelu -att_out=1 -pool=sum -logdir=log/att_neibor_lrelu -data=DD
-#python3 main.py -fold=$i -softmax=neibor -adj_norm=none -dnorm=1 -relu=lrelu -att_out=1 -pool=sum -logdir=log/att_neibor_lrelu -gpu=$3 -data=PROTEINS
-#python3 main.py -fold=$i -softmax=neibor -adj_norm=none -dnorm=1 -relu=lrelu -att_out=1 -pool=sum -logdir=log/att_neibor_lrelu -gpu=$3 -data=MUTAG
-#python3 main.py -fold=$i -softmax=neibor -adj_norm=none -dnorm=1 -relu=lrelu -att_out=1 -pool=sum -logdir=log/att_neibor_lrelu -gpu=$3 -data=COLLAB
-#python3 main.py -fold=$i -softmax=neibor -adj_norm=diag -dnorm=1 -relu=lrelu -att_out=1 -pool=sum -logdir=log/att_neibor_lrelu -gpu=$3 -data=COLLAB
-#python3 main.py -fold=$i -softmax=neibor -adj_norm=diag -dnorm=1 -num_layers=4 -gcn_layers=8 -relu=lrelu -att_out=1 -pool=sum -logdir=log/att_neibor_lrelu -gpu=$3 -data=REDDIT-BINARY
+python3 main.py -fold=$i -softmax=neibor -adj_norm=none -dnorm=1 -relu=lrelu -att_out=1 -pool=sum -logdir=log/att_neibor_lrelu -gpu=$3 -data=NCI1
+python3 main.py -fold=$i -softmax=neibor -adj_norm=none -dnorm=1 -relu=lrelu -att_out=1 -pool=sum -logdir=log/att_neibor_lrelu -gpu=$3 -data=PROTEINS
+python3 main.py -fold=$i -softmax=neibor -adj_norm=none -dnorm=1 -relu=lrelu -att_out=1 -pool=sum -logdir=log/att_neibor_lrelu -gpu=$3 -data=MUTAG
+python3 main.py -fold=$i -softmax=neibor -adj_norm=none -dnorm=1 -relu=lrelu -att_out=1 -pool=sum -logdir=log/att_neibor_lrelu -gpu=$3 -data=COLLAB
 done
